0a17a656f6d693db81ed1a10071de00a40695f56
Marija Vrljanac <marijavrljanac@gmail.com>
2022-02-02 16:52:26 +0100

    Dodat dizajn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from "./components/NavBar";
+import Products from "./components/Products";
+import Cart from "./components/Cart";
+import { useState } from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom"; //Routes je kao switch(), Route je kao case
 
 function App() {
   return (
